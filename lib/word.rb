d727b0ab6a2b0cf44e90fb3ba8ab8d7ff3bfe717
nursetta <n_ursetta@yahoo.com>
2016-07-25 10:56:29 -0600

    solution

--- lib/word.rb
+++ lib/word.rb
@@ -1,9 +1,31 @@
+VOWELS = ["a", "e", "i", "o", "u"]
+
+
 class Word
   def initialize(original_word)
     @original_word = original_word
   end
 
+	def piglatinize 
+		
+		VOWELS.each do |vowel|
+			if vowel == @original_word.chars[0]
+				return @original_word + "way"
+			end		
+		end	
+		VOWELS.each do |vowel|
+			if vowel == @original_word.chars[1]
+				return @original_word.chars.rotate.join.to_s + "ay"
+			end		
+		end	
+
+		return @original_word.chars.rotate(2).join.to_s + "ay"
+			
+	end
+
+
   def original_word
     return @original_word.downcase
   end
 end
+
